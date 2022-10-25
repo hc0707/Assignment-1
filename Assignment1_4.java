929262b7484707dd048c478c9ab262bd49987e8e
hc0707 <harshc0707@gmail.com>
2022-10-25 14:45:08 +0530

    Question4

---
+++ Assignment1_4.java
@@ -0,0 +1,21 @@
+public class Assignment1_4 {
+    public static void main(String[] args) {
+
+    int n=13;
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) {
+            if (i-j>=n/2||i+j>=3*(n-1)/2) 
+            System.out.print("* ");
+            
+            else
+            System.out.print("  ");
+        }
+        
+        System.out.println("");
+           
+        
+    }
+    }
+}
